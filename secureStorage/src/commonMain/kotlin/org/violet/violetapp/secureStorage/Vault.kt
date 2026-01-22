a3ba839482bcbc206d8e3a7d1f2fcee57b11cf38
Gleb Gutnik <glebgytnik@gmail.com>
2026-01-22 09:28:45 +0700

    Remove unused module

--- secureStorage/src/commonMain/kotlin/org/violet/violetapp/secureStorage/Vault.kt
+++
@@ -1,9 +0,0 @@
-package org.violet.violetapp.secureStorage
-
-interface Vault {
-    suspend fun string(key: String): String?
-
-    suspend fun saveStr(key: String, value: String)
-
-    suspend fun clear()
-}
