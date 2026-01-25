8a7d5ec649f7e4574a8ffb9f46a650d29863dac9
Gleb Gutnik <glebgytnik@gmail.com>
2026-01-25 15:27:32 +0700

    Improve web links handling

--- composeApp/src/commonMain/kotlin/org/sportsradar/sportsradarapp/common/presentation/components/SnackbarScaffold.kt
+++ composeApp/src/commonMain/kotlin/org/sportsradar/sportsradarapp/common/presentation/components/SnackbarScaffold.kt
@@ -2,14 +2,12 @@ package org.sportsradar.sportsradarapp.common.presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
-import org.sportsradar.uiKit.theme.LocalSportsRadarTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import org.sportsradar.sportsradarapp.common.presentation.LocalScreenSize
+import org.sportsradar.uiKit.theme.LocalSportsRadarTheme
 
 @Composable
 fun SnackbarScaffold(
